123c86335465b57e74e510f68d4ede61214e2ebe
unknown <nilanjanac873@gmail.com>
2022-04-11 10:33:04 +0530

    [Nilanjana] updated services

--- src/app/Services/dataShare/datashare.service.ts
+++ src/app/Services/dataShare/datashare.service.ts
@@ -8,9 +8,25 @@ export class DatashareService {
   private info = new Subject<any>();
   public store = this.info.asObservable();
 
+  private info1 = new Subject<any>();
+  public store1 = this.info1.asObservable();
+
+  private info2 = new Subject<any>();
+  public store2 = this.info2.asObservable();
+
   constructor() { }
 
   updateData(text:any){
     this.info.next(text);
   }
+
+  updateData1(text:any){
+    this.info1.next(text);
+  }
+  
+  updateData2(text:any){
+    this.info2.next(text);
+  }
+
 }
+
